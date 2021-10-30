0d14a2596b50d972f229db3d30e35d8bfa0bf12a
ichitac <78163643+ichitac@users.noreply.github.com>
2021-10-30 20:40:38 +0900

    Add files via upload

--- lib/word_synth.rb
+++ lib/word_synth.rb
@@ -1,5 +1,15 @@
 class WordSynth
-    def play(original_words)
+    def initialize
+        @effect = []
+    end
 
+    def add_effect(effect)
+        @effect << effect
+    end
+
+    def play(original_words)
+        @effect.inject(original_words) do |words, effect|
+            effect.call(words)
+        end
     end
 end
