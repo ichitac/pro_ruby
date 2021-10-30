c8223ab1378963a888bbc29a19b3caf4882fe459
ichitac <78163643+ichitac@users.noreply.github.com>
2021-10-30 20:40:11 +0900

    Add files via upload

--- lib/effect.rb
+++ lib/effect.rb
@@ -7,7 +7,13 @@ module Effects
 
     def self.echo(rate)
         ->(words) do
-            words,chars.map{ |w| w = " " ? " " | w * rate }.join
+            words.chars.map{ |w| w == ' ' ? w : w * rate }.join
+        end
+    end
+
+    def self.loud(level)
+        ->(words) do
+            words.split(" ").map{ |w| w.upcase! + "!" * level }.join(" ")
         end
     end
 end
